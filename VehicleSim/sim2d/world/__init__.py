b6334f835b3d82214f3443fc160abe0260dc0510
Carolus <109283167+YGY10@users.noreply.github.com>
2026-07-21 15:12:56 +0800

    VehicleSim: export road deformation APIs

--- VehicleSim/sim2d/world/__init__.py
+++ VehicleSim/sim2d/world/__init__.py
@@ -1,19 +1,25 @@
-from sim2d.world.state import (
-    WorldState,
+from sim2d.world.road_deformation import (
+    RoadDeformationConfig,
+    deform_points,
+    deform_pose,
+    deform_road_network,
 )
-
-from sim2d.world.world import (
-    World,
-)
-
+from sim2d.world.road_geometry import WorldLaneGeometry
+from sim2d.world.state import WorldState
 from sim2d.world.traffic_signal import (
     TrafficLightState,
     WorldTrafficSignal,
 )
+from sim2d.world.world import World
 
 __all__ = [
+    "RoadDeformationConfig",
+    "TrafficLightState",
     "World",
+    "WorldLaneGeometry",
     "WorldState",
     "WorldTrafficSignal",
-    "TrafficLightState",
+    "deform_points",
+    "deform_pose",
+    "deform_road_network",
 ]
