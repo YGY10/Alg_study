54c571fb42b23407f5d1d939c4046c950ebbd6b2
Carolus <109283167+YGY10@users.noreply.github.com>
2026-07-21 16:29:45 +0800

    VehicleSim: export perception interfaces

---
+++ VehicleSim/sim2d/perception/__init__.py
@@ -0,0 +1,19 @@
+from sim2d.perception.ground_truth import GroundTruthLocalPerception
+from sim2d.perception.types import (
+    PerceivedLaneSegment,
+    PerceivedObject,
+    PerceivedTrafficSignal,
+    PerceptionConfig,
+    PerceptionSnapshot,
+    PlanningInput,
+)
+
+__all__ = [
+    "GroundTruthLocalPerception",
+    "PerceivedLaneSegment",
+    "PerceivedObject",
+    "PerceivedTrafficSignal",
+    "PerceptionConfig",
+    "PerceptionSnapshot",
+    "PlanningInput",
+]
