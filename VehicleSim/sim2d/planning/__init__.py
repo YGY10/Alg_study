495d7522e50de4d96cf7067a7e6cccd78224727e
Carolus <109283167+YGY10@users.noreply.github.com>
2026-07-21 16:32:57 +0800

    VehicleSim: export planner perception input

--- VehicleSim/sim2d/planning/__init__.py
+++ VehicleSim/sim2d/planning/__init__.py
@@ -1,15 +1,13 @@
-from sim2d.planning.base import (
-    Planner,
-)
-from sim2d.planning.bezier_planner import (
-    BezierPlanner,
-)
-from sim2d.planning.simple_planner import (
-    SimplePlanner,
-)
+from sim2d.perception import PlanningInput
+from sim2d.planning.base import Planner
+from sim2d.planning.bezier_planner import BezierPlanner
+from sim2d.planning.simple_planner import SimplePlanner
+from sim2d.types import PlanResult
 
 __all__ = [
     "Planner",
+    "PlanningInput",
+    "PlanResult",
     "BezierPlanner",
     "SimplePlanner",
 ]
