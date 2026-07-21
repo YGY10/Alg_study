2c7eac449621616c1027342494df2e317838b078
Carolus <109283167+YGY10@users.noreply.github.com>
2026-07-21 15:13:08 +0800

    VehicleSim: enable road deformation GUI extension

--- VehicleSim/main.py
+++ VehicleSim/main.py
@@ -1,4 +1,8 @@
-from sim2d.gui.main_window import main
+from sim2d.gui.road_layer_extension import install
+
+install()
+
+from sim2d.gui.main_window import main  # noqa: E402
 
 
 if __name__ == "__main__":
