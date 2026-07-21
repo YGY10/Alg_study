687f727f05d9909345e6762dadeb2c0e42998029
Carolus <109283167+YGY10@users.noreply.github.com>
2026-07-21 16:30:44 +0800

    VehicleSim: install planner perception contract

--- VehicleSim/sim2d/core/__init__.py
+++ VehicleSim/sim2d/core/__init__.py
@@ -10,29 +10,17 @@ from sim2d.core.collision import (
     oriented_box_corners,
     vehicle_corners,
 )
-from sim2d.core.dynamics import (
-    KinematicBicycleModel,
-)
-from sim2d.core.environment import (
-    DrivingEnv,
-    EnvironmentConfig,
-)
+from sim2d.core.dynamics import KinematicBicycleModel
+from sim2d.core.environment import DrivingEnv, EnvironmentConfig
+from sim2d.core.perception_extension import PlannerProtocol, install
+
+install()
 
 __all__ = [
-    "KinematicBicycleModel",
-    "circle_box_clearance",
-    "circle_box_collision",
-    "collision_with_obstacle",
-    "has_collision",
-    "minimum_clearance",
-    "obstacle_clearance",
-    "oriented_box_clearance",
-    "oriented_box_collision",
-    "oriented_box_corners",
-    "vehicle_corners",
     "DrivingEnv",
     "EnvironmentConfig",
     "KinematicBicycleModel",
+    "PlannerProtocol",
     "circle_box_clearance",
     "circle_box_collision",
     "collision_with_obstacle",
