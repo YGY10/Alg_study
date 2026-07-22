76c78b2559d9dea606e1c4908e5e9673b544a94b
Carolus <109283167+YGY10@users.noreply.github.com>
2026-07-22 10:10:37 +0800

    VehicleSim: export semantic traffic actor API

--- VehicleSim/sim2d/world/__init__.py
+++ VehicleSim/sim2d/world/__init__.py
@@ -6,6 +6,14 @@ from sim2d.world.road_deformation import (
 )
 from sim2d.world.road_geometry import WorldLaneGeometry
 from sim2d.world.state import WorldState
+from sim2d.world.traffic_actor import (
+    LargeVehicleObstacle,
+    PedestrianObstacle,
+    SmallCarObstacle,
+    TrafficActor,
+    TrafficActorType,
+    create_traffic_actor,
+)
 from sim2d.world.traffic_signal import (
     TrafficLightState,
     WorldTrafficSignal,
@@ -13,12 +21,18 @@ from sim2d.world.traffic_signal import (
 from sim2d.world.world import World
 
 __all__ = [
+    "LargeVehicleObstacle",
+    "PedestrianObstacle",
     "RoadDeformationConfig",
+    "SmallCarObstacle",
+    "TrafficActor",
+    "TrafficActorType",
     "TrafficLightState",
     "World",
     "WorldLaneGeometry",
     "WorldState",
     "WorldTrafficSignal",
+    "create_traffic_actor",
     "deform_points",
     "deform_pose",
     "deform_road_network",
