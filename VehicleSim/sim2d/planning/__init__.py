94ef01fe5d304ddc65ca991213f21b1a56066a74
Carolus <109283167+YGY10@users.noreply.github.com>
2026-07-22 17:41:34 +0800

    在规划模块中注册时空联合规划器

--- VehicleSim/sim2d/planning/__init__.py
+++ VehicleSim/sim2d/planning/__init__.py
@@ -2,6 +2,10 @@ from sim2d.perception import PlanningInput
 from sim2d.planning.base import Planner
 from sim2d.planning.bezier_planner import BezierPlanner
 from sim2d.planning.simple_planner import SimplePlanner
+from sim2d.planning.spatiotemporal_planner import (
+    SpatiotemporalPlanner,
+    SpatiotemporalPlannerConfig,
+)
 from sim2d.types import PlanResult
 
 __all__ = [
@@ -10,4 +14,6 @@ __all__ = [
     "PlanResult",
     "BezierPlanner",
     "SimplePlanner",
+    "SpatiotemporalPlanner",
+    "SpatiotemporalPlannerConfig",
 ]
